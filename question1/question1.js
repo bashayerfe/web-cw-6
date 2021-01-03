dc37317d106ab0d83cfb84149b8523d4c1e913e6
bashayerfe <76617886+bashayerfe@users.noreply.github.com>
2021-01-03 21:35:05 +0300

    done bashayer

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,19 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+
+function sayHello(name) {
+
+  if (name==null)
+ {
+console.log(`يرجى ادخال الاسم`); 
+}
+
+else if (name!=null)
+ {
+    console.log(`مرحبا انا ${name}`);
+
+}
+
+}
+
+sayHello();
+sayHello(`بشاير`);
